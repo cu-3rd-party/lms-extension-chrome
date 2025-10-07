d2c48e3f1fbc255d57b60eba487410a2effad050
Igor <igor.mamaev1@gmail.com>
2025-10-07 14:09:15 +0300

    implement oled theme with simple switch in popup.html

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,14 @@
 // popup.js (кросс-браузерная версия)
 
 const themeToggle = document.getElementById('theme-toggle');
+const oledToggle = document.getElementById('oled-toggle');
 
 // 1. При открытии popup, получить текущее состояние и обновить переключатель
 // Используем browser.storage, который возвращает Promise, понятный полифиллу
-browser.storage.sync.get('themeEnabled').then((data) => {
-    // !!data.themeEnabled превратит undefined или false в false, а true в true
+browser.storage.sync.get(['themeEnabled', 'oledEnabled']).then((data) => {
     themeToggle.checked = !!data.themeEnabled;
+    oledToggle.checked = !!data.oledEnabled;
+    oledToggle.disabled = !themeToggle.checked;
 });
 
 // 2. При клике на переключатель, сохранить новое состояние.
@@ -14,4 +16,11 @@ browser.storage.sync.get('themeEnabled').then((data) => {
 themeToggle.addEventListener('change', () => {
     const isEnabled = themeToggle.checked;
     browser.storage.sync.set({ themeEnabled: isEnabled });
+    oledToggle.disabled = !isEnabled;
+});
+
+// 3. OLED toggle controls variant of dark
+oledToggle.addEventListener('change', () => {
+    const isOled = oledToggle.checked;
+    browser.storage.sync.set({ oledEnabled: isOled });
 });
